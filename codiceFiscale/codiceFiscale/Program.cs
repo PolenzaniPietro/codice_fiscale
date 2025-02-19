baf70467ef439210ba2bb4d4a9d7f2a563ac0377
PolenzaniPietro <polenzani.pietro@franchettisalviani.net>
2025-02-19 09:54:49 +0100

    seconda versione(solo cognome)

--- codiceFiscale/codiceFiscale/Program.cs
+++ codiceFiscale/codiceFiscale/Program.cs
@@ -10,16 +10,16 @@ string giorno = Console.ReadLine();
 string mese = Console.ReadLine();
 string anno = Console.ReadLine();
 Console.WriteLine("di che sesso sei?");
-string sesso= Console.ReadLine();
-string codiceFiscale=" ";
-char[] consonanti = { 'b', 'c', 'd','f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w' ,'x' , 'y' , 'z'};
-char[] vocali = { 'a' , 'e' , 'i' , 'o' , 'u' };
-char[] consonantiC= new char[3];
-char[] vocaliC= new char[2];
-int contaVocali=0, contaConsonanti=0;
-for (int i=0; i<nome.Length; i++)
+string sesso = Console.ReadLine();
+string codiceFiscale = " ";
+char[] consonanti = { 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z' };
+char[] vocali = { 'a', 'e', 'i', 'o', 'u' };
+char[] consonantiC = new char[3];
+char[] vocaliC = new char[2];
+int contaVocali = 0, contaConsonanti = 0;
+for (int i = 0; i < nome.Length; i++)
 {
-    for(int j=0; j<consonanti.Length; j++)
+    for (int j = 0; j < consonanti.Length; j++)
     {
         if (nome[i] == consonanti[j])
         {
@@ -32,10 +32,10 @@ for (int i=0; i<nome.Length; i++)
             {
                 i = nome.Length;
             }
-            
-        }   
+
+        }
     }
-    for(int j=0; j<vocali.Length; j++)
+    for (int j = 0; j < vocali.Length; j++)
     {
         if (nome[i] == vocali[j])
         {
@@ -48,7 +48,7 @@ for (int i=0; i<nome.Length; i++)
             {
                 i = nome.Length;
             }
-            
+
         }
     }
     //CREAZIONE DEL COGNOME
@@ -56,9 +56,9 @@ for (int i=0; i<nome.Length; i++)
     {
         codiceFiscale += consonantiC[0] + consonantiC[1] + consonantiC[2];
     }
-    else if(contaConsonanti == 2)
+    else if (contaConsonanti == 2)
     {
-        codiceFiscale += consonantiC[0] + consonantiC[1]+ vocaliC[0];
+        codiceFiscale += consonantiC[0] + consonantiC[1] + vocaliC[0];
     }
     else if (contaConsonanti == 1)
     {
@@ -73,7 +73,7 @@ for (int i=0; i<nome.Length; i++)
     }
     else if (contaConsonanti == 0)
     {
-        codiceFiscale += vocaliC[0] + vocaliC[1] + "x"; 
+        codiceFiscale += vocaliC[0] + vocaliC[1] + "x";
     }
 }
 // CREAZIONE DEL NOME
